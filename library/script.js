af17d0dbc483b30659a441650ce9ffa841c2f2eb
daniel mestawat <dannymamusha@gmail.com>
2020-12-13 05:03:09 -0500

    update three

--- library/script.js
+++ library/script.js
@@ -2,12 +2,37 @@ console.log("am i linked");
 
 // display of time using moment js using time interval.
 
-var currentDayNew =document.getElementById("currentDay");
-setInterval(function(){
-    var now = moment();
-    var visibleTime = now.format('dddd,MMMM,Do');
+// var currentDayNew =document.getElementById("currentDay");
+// setInterval(function(){
+//     var now = moment();
+//     var visibleTime = now.format('dddd,MMMM,Do');
     
    
-  currentDayNew.textContent = visibleTime;
+//   currentDayNew.textContent = visibleTime;
+  
+// },1000)
+// // var now = moment().format('MMMM Do YYYY, h:mm:ss a');
+// console.log(currentDayNew);
 
-},1000)
+var toDay = moment().format('dddd,MMMM,Do');
+
+$("#currentDay").append(toDay);
+// changing the current time to one digit.
+var now =  parseInt(moment().format('HH'));
+console.log(now);
+
+// creating a variable for current time
+
+
+
+$("input").each( function () { 
+     var scheduleTime = parseInt($(this).attr("data-hour"));
+     if (scheduleTime < now){
+         $(this).addClass("past");
+     }
+      else if (scheduleTime > now) {
+         $(this).addClass("future")
+     }
+      else $(this).addClass("present")
+     
+})
